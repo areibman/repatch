9b875a586e1f7480766ae6516e757e0147ac3215
Alex Reibman <meta.alex.r@gmail.com>
2025-10-24 15:30:04 -0700

    Externalize webpack loaders for Remotion

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,18 @@ const nextConfig: NextConfig = {
   /* config options here */
   webpack: (config, { isServer }) => {
     if (isServer) {
-      // Externalize Remotion packages and their dependencies
-      // These are server-side only and shouldn't be bundled by Next.js
+      // Externalize Remotion packages - they'll load their own dependencies at runtime
       config.externals = config.externals || [];
       config.externals.push({
         '@remotion/bundler': 'commonjs @remotion/bundler',
         '@remotion/renderer': 'commonjs @remotion/renderer',
         '@remotion/lambda': 'commonjs @remotion/lambda',
+        '@remotion/tailwind-v4': 'commonjs @remotion/tailwind-v4',
         'esbuild': 'commonjs esbuild',
+        // Don't externalize webpack loaders - they need to be available
+        'style-loader': 'commonjs style-loader',
+        'css-loader': 'commonjs css-loader',
+        'postcss-loader': 'commonjs postcss-loader',
       });
     }
     return config;
